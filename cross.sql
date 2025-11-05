0d81f2302233d26766572c484318163e959511e4
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-11-05 11:50:15 +0600

    Join all command

---
+++ cross.sql
@@ -0,0 +1,28 @@
+-- Active: 1762065643192@@127.0.0.1@5000@ph
+
+
+CREATE Table departments(
+    dept_id int,
+    dept_name VARCHAR(50)
+)
+
+create TABLE employees(
+    emp_id int,
+    emp_name VARCHAR(50),
+    dept_id int
+)
+
+SELECT * from departments;
+
+SELECT * from employees
+
+INSERT INTO departments VALUES
+(101,'Human resource'),
+(102,'Marketing')
+
+
+INSERT into employees VALUES
+(1,'John Doe',101),
+(2,'jabe Smith',102)
+
+
--- join.sql
+++ join.sql
@@ -5,9 +5,31 @@ SELECT * from "user";
 
 SELECT * from post;
 
+ALTER Table post 
+    alter COLUMN user_id DROP NOT NULL
+
+INSERT INTO post(id,title,user_id) VALUES(5,'test',NULL);
 
 
 SELECT title,user_name FROM post
     JOIN "user" ON post.user_id = "user".id ;
+
+
+
+SELECT * FROM post
+    LEFT JOIN "user" ON post.user_id = "user".id ;
+
+SELECT * FROM "user"
+    LEFT JOIN post ON "user".id = post.id ;
+
+
+SELECT * FROM post
+    RIGHT JOIN "user" ON post.user_id = "user".id ;
+
+
+
+SELECT * FROM post
+    FULL JOIN "user" ON post.user_id = "user".id ;
+
 
 
